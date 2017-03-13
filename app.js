1d3219dc3ca5143e83f7eeca9b75c0329b979524
lalogf <lalogf@gmail.com>
2017-03-13 01:46:48 -0500

    eventos, iniciativas, noticias

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 
-$(".welcome-scroll").on('click',function(event){
-	event.preventDefault();
-	$('html, body').animate({
-		scrollTop: $("#welcome").offset().top
-	}, 800);
-});
-$(".mobile-hb").on("click",function(e){
-	e.preventDefault();
-	$(".menu-mobile").toggle();
-});
+var ready = function (){
+	$(".welcome-scroll").on('click',function(event){
+		event.preventDefault();
+		$('html, body').animate({
+			scrollTop: $("#welcome").offset().top
+		}, 800);
+	});
+	$(".mobile-hb").on("click",function(e){
+		e.preventDefault();
+		$(".menu-mobile").toggle();
+	});
+
+	$(".btn-concytec-filter").on('click',function(e){
+		e.preventDefault();
+		var filter = $(this).attr('id');
+		if (filter === "all") {
+			$(".thumbnail-aliados-container").each(function(){
+				$(this).removeClass('filtered');
+				$(this).addClass('visible');
+			});
+		} else {
+			$(".thumbnail-aliados-container").each(function(){
+				if (!($(this).hasClass(filter))){
+					$(this).removeClass('visible');
+					$(this).addClass('filtered');
+				} else {
+					$(this).removeClass('filtered');
+					$(this).addClass('visible');
+				}
+			});
+		};
+	});
+};
+
+$(document).ready(ready)
+$(document).on('page:load',ready)
